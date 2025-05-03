42d1964b77fda191226719f286737da93d2e267f
dennisP <dennis.perez@epn.edu.ec>
2025-05-02 21:31:28 -0500

    Se cambia de DbContext a IdentityDbContext

--- DASI_2025A/Backend/Data/ApplicationDbContext.cs
+++ DASI_2025A/Backend/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 // Backend/Data/ApplicationDbContext.cs
+using Backend.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Backend
 {
-  public class ApplicationDbContext : DbContext
+  public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
   {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
