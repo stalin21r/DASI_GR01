583f9088a8e524caa4eed7596b77a6fbc4f13d26
dennisP <dennis.perez@epn.edu.ec>
2025-05-02 21:38:10 -0500

    Controlador para la autenticacion. Usa el servicio IAuthService

---
+++ DASI_2025A/Backend/Controllers/AuthController.cs
@@ -0,0 +1,57 @@
+﻿using Backend.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Dto;
+
+namespace Backend.Controllers
+{
+    [ApiController, Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _auth;
+
+        public AuthController(IAuthService auth) 
+        {
+            _auth = auth;    
+        }
+
+        // Registro de un usuario
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto registerDto)
+        {
+            try
+            {
+                var response = await _auth.RegisterAsync(registerDto);
+                return Created(string.Empty, response);
+
+            }
+            catch (BadHttpRequestException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error del servidor al registrar al usuario {ex.Message}");
+            }
+        }
+
+        // Login de un usuario
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            try
+            {
+                var response = await _auth.LoginAsync(loginDto);
+                return Created(string.Empty, response);
+
+            }
+            catch (BadHttpRequestException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error del servidor al hacer login {ex.Message}");
+            }
+        }
+    }
+}
