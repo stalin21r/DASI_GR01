ea1443c899bcf8c6e9cbce6369ec2a9ca2c77632
Miguel Pastuña <david.rmig@gmail.com>
2025-05-03 19:13:44 -0500

    Add new attributes to better identify the user model

--- DASI_2025A/Backend/Data/Models/ApplicationUser.cs
+++ DASI_2025A/Backend/Data/Models/ApplicationUser.cs
@@ -1,13 +1,42 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Backend.Data.Models
 {
     public class ApplicationUser : IdentityUser
     {
         // ----- Datos propios -----
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public string Cedula { get; set; } = string.Empty;
-        public string UniqueScoutId { get; set; } = string.Empty;
-    }
+        [Required]
+		public string FirstName { get; set; } = string.Empty;
+		[Required]
+		public string LastName { get; set; } = string.Empty;
+		[Required]
+		public DateTime DateOfBirth { get; set; }
+		[Required]
+		public string Cedula { get; set; } = string.Empty;
+		[Required]
+		public string UniqueScoutId { get; set; } = string.Empty;
+		[Required]
+		[EmailAddress]
+		[PersonalData]
+		public override string? Email
+		{
+			get => base.Email;
+			set
+			{
+				base.Email = value;
+				base.UserName = value; // Establecer el email como UserName
+			}
+		}
+
+		[Required]
+		public bool Active { get; set; }
+		// FK a Occupation
+		[Required]
+		public int OccupationFk { get; set; }
+
+		[ForeignKey("OccupationFk")]
+		public OccupationEntity? Occupation { get; set; }
+	}
 }
