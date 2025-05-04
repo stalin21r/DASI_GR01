a0d61ff05c28b2856d45388d4d6c53f54457685d
Miguel Pastuña <david.rmig@gmail.com>
2025-05-03 19:45:14 -0500

    Add to UserDto

--- DASI_2025A/Shared/Dto/UserDto.cs
+++ DASI_2025A/Shared/Dto/UserDto.cs
@@ -1,9 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Shared
+namespace Shared;
+
+public class UserDto
 {
-    class UserDto
-    {
-		Email
-	}
+	[Required(ErrorMessage = "El nombre es requerido"), StringLength(200)]
+	public string FirstName { get; set; } = string.Empty;
+	[Required(ErrorMessage = "El apellido es requerido"), StringLength(200)]
+	public string LastName { get; set; } = string.Empty;
+	[Required(ErrorMessage = "La fecha de nacimiento es requerida")]
+	public DateTime DateOfBirth { get; set; }
+	[Required]
+	public string Cedula { get; set; } = string.Empty;
+	[Required]
+	public string UniqueScoutId { get; set; } = string.Empty;
+	[Required]
+	[EmailAddress]
+	public string? Email { get; set; }
+	[Required(ErrorMessage = "El estado es requerido")]
+	public bool Active { get; set; }
+	[Required]
+	public String? Occupation { get; set; }
 }
