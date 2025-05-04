663393ddbd56aa8e74e9ab3119cc0c0e43cf32d9
Miguel Pastuña <david.rmig@gmail.com>
2025-05-03 19:23:52 -0500

    Modify bug in encapsulation by OccupationEntity

--- DASI_2025A/Backend/Data/Models/OccupationEntity.cs
+++ DASI_2025A/Backend/Data/Models/OccupationEntity.cs
@@ -2,15 +2,17 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using Backend.Data.Models;
 
-namespace Backend;
-public class OccupationEntity : AuditableEntity
+namespace Backend
 {
-	[Key]
-	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-	public int Id { get; set; }
-	[Required]
-	[MaxLength(50)]
-	public string? Name { get; set; }
+	public class OccupationEntity : AuditableEntity
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+		[Required]
+		[MaxLength(50)]
+		public string? Name { get; set; }
 
-	public ICollection<ApplicationUser>? Users { get; set; }
+		public ICollection<ApplicationUser>? Users { get; set; }
+	}
 }
