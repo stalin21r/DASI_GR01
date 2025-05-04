6ff1c89ecb6183030a5954a439d2d784b87c754f
Miguel Pastuña <david.rmig@gmail.com>
2025-05-03 19:00:50 -0500

    Edit namespace by UserController

--- DASI_2025A/Backend/Controllers/UserController.cs
+++ DASI_2025A/Backend/Controllers/UserController.cs
@@ -2,7 +2,7 @@
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace Backend.Controllers
+namespace Backend
 {
     [Route("api/[controller]")]
     [ApiController]
