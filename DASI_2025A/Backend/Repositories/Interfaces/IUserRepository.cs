0820063a2a4fae5794a97b3fa7cc9cecffd0796e
Miguel Pastuña <david.rmig@gmail.com>
2025-05-03 20:14:20 -0500

    Add new interface by create funtions that been describe in UserRepository

---
+++ DASI_2025A/Backend/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Backend.Data.Models;
+using Shared;
+
+namespace Backend;
+public interface IUserRepository
+{
+	Task<UserDto> CreateAsync(UserDto userDto);
+	Task<IEnumerable<UserDto>> GetAllAsync();
+	Task<UserDto?> GetAsync(string id);
+	Task<UserDto?> GetByEmailAsync(string email);
+	Task<UserDto> UpdateAsync(UserDto userDto);
+	Task<bool> DeleteAsync(string id);
+	Task<bool> AssignRoleAsync(ApplicationUser userId, string roleName);
+}
