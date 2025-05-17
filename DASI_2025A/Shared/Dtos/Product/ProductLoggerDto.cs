c0024b213c21b7559e1342dc41aa3cc5a766e82d
dennisP <dennis.perez@epn.edu.ec>
2025-05-17 15:34:17 -0500

    Resolucion de conflictos con rama development

--- DASI_2025A/Shared/Dtos/Product/ProductLoggerDto.cs
+++ DASI_2025A/Shared/Dtos/Product/ProductLoggerDto.cs
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Shared
-{
-    public class ProductLoggerDto
-    {
-        public int Id { get; set; }
-
-        [Required]
-        public int ProductId { get; set; }
-
-        [Required]
-        public int UserId { get; set; }
-
-        [Required(ErrorMessage = "Action es requerido")]
-        [StringLength(10, ErrorMessage = "Action debe tener menos de 10 caracteres")]
-        public string Action { get; set; } = string.Empty;
-
-        [StringLength(1000, ErrorMessage = "La descripcion debe tener menos de 1000 caracteres")]
-        public string Description { get; set; } = string.Empty;
-
-        [Required(ErrorMessage = "QuantityBefore es requerido")]
-        [Range(0, int.MaxValue, ErrorMessage = "QuantityBefore debe ser mayor o igual a 0")]
-        public int QuantityBefore { get; set; }
-
-        [Required(ErrorMessage = "QuantityAfter es requerido")]
-        [Range(0, int.MaxValue, ErrorMessage = "QuantityAfter debe ser mayor o igual a 0")]
-        public int QuantityAfter { get; set; }
-
-
-    }
-=======
 ﻿using System.ComponentModel.DataAnnotations;
 
 namespace Shared
@@ -71,5 +33,4 @@ namespace Shared
     public string? ProductName { get; set; }
 
   }
->>>>>>> origin/development
 }
