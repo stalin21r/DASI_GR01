a6a5722f370f21319394da7feaf00a44f582de74
dennisP <dennis.perez@epn.edu.ec>
2025-05-17 15:32:05 -0500

    Resolucion de conflictos con rama development

--- DASI_2025A/Backend/Repositories/ProductLoggerRepository.cs
+++ DASI_2025A/Backend/Repositories/ProductLoggerRepository.cs
@@ -1,44 +1,6 @@
-<<<<<<< HEAD
-﻿using Microsoft.EntityFrameworkCore;
-using Shared;
-
-
-namespace Backend
-{
-    public class ProductLoggerRepository : IProductLoggerRepository
-    {
-        private readonly ApplicationDbContext _context;
-
-        public ProductLoggerRepository(ApplicationDbContext context)
-        {
-            _context = context;
-        }
-
-        public async Task<ProductLoggerDto> CreateAsync(ProductLoggerDto productLoggerDto)
-        {
-            var entity = new ProductLoggerEntity
-            {
-                Action = productLoggerDto.Action,
-                Description = productLoggerDto.Description,
-                QuantityBefore = productLoggerDto.QuantityBefore,
-                QuantityAfter = productLoggerDto.QuantityAfter,
-                ProductFk = productLoggerDto.ProductId,
-                UserFk = productLoggerDto.UserId
-            };
-
-            _context.Add(entity);
-            await _context.SaveChangesAsync();
-
-            return productLoggerDto;
-        }
-
-
-    }
-=======
 ﻿using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 using Shared;
-
 
 namespace Backend;
 
@@ -138,6 +100,4 @@ public class ProductLoggerRepository : IProductLoggerRepository
 
 		return productLoggerDto;
 	}
-
->>>>>>> origin/development
 }
