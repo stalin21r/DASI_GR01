bd773f3329f193dea14ca0acc184c5f20cd073d1
dennisP <dennis.perez@epn.edu.ec>
2025-05-18 19:19:49 -0500

    Enums para algunas entidades

---
+++ DASI_2025A/Backend/Data/Models/Enums/OrderStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend
+{
+    public enum OrderStatus
+    {
+        Pending = 0,
+        Paid = 1,
+        Cancelled = 2
+    }
+}
---
+++ DASI_2025A/Backend/Data/Models/Enums/TransactionType.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend
+{
+    public enum TransactionType
+    {
+        Credit = 0,
+        Debit = 1,
+        Refund = 2
+    }
+}
