ead1b5a6750178ecd242f9b1932c6962be613857
dennisP <dennis.perez@epn.edu.ec>
2025-05-18 19:21:00 -0500

    Entidad para registrar las ordenes generadas por un cliente

---
+++ DASI_2025A/Backend/Data/Models/OrderEntity.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend
+{
+    public class OrderEntity : AuditableEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public required string OrderNote { get; set; }
+
+        [Required]
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public required decimal TotalAmount { get; set; }
+
+        public OrderStatus Status { get; set; } = OrderStatus.Pending; // Tipo enum OrderStatus
+
+        public bool IsActive { get; set; } = true;
+
+        [Required]
+        public string UserId { get; set; } = null!;
+        [ForeignKey("UserFk")]
+        public ApplicationUser? User { get; set; }
+
+        public List<OrderDetailEntity> Details { get; set; } = new List<OrderDetailEntity>();
+
+    }
+}
