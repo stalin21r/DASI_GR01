92557526d5bdc3e0497f021f56b1c478f58a2ef6
dennisP <dennis.perez@epn.edu.ec>
2025-05-21 19:44:11 -0500

    Enum para una Orden o Pago

---
+++ DASI_2025A/Shared/Dtos/Order/OrderDetailDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    internal class OrderDetailDto
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal SubTotal { get; set; }
+        public bool IsActive { get; set; }
+
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+    }
+}
---
+++ DASI_2025A/Shared/Dtos/Order/OrderDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Shared
+{
+    internal class OrderDto
+    {
+        public int? Id { get; set; }
+        public string OrderNote { get; set; } = string.Empty;
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public Status Status { get; set; } = Status.Pending;
+        public bool? IsActive { get; set; } = true;
+        public bool? IsReverted { get; set; } = false;
+        public string? UserId { get; set; } = string.Empty;
+
+        public List<OrderDetailDto> Details { get; set; } = new(); // DTO anidado para detalles
+
+        //El DTO de Order debe recibir:
+        //    OrderNote
+        //    OrderDate
+        //    TotalAmount
+        //    UserId
+    }
+}
