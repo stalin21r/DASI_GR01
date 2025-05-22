e6b62a57b60a0da14885f29f7bf36ff202841da3
dennisP <dennis.perez@epn.edu.ec>
2025-05-21 19:44:54 -0500

    Dto para Payment

---
+++ DASI_2025A/Shared/Dtos/Payment/PaymentDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    internal class PaymentDto
+    {
+        public int Id { get; set; }
+
+        public decimal AmountDue { get; set; }
+
+        public PaymentMethod PaymentMethod { get; set; } = PaymentMethod.AccountBalance;  
+
+        public Status Status { get; set; } = Status.Pending;  
+
+        public DateTime? PaidAt { get; set; }
+
+        public string? ComprobanteUrl { get; set; }
+
+        public int? ParentOrderId { get; set; }
+    }
+}
