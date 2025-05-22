1d6dd678781c3c90bc2876aded7359418b7d4780
dennisP <dennis.perez@epn.edu.ec>
2025-05-21 19:41:01 -0500

    Repositorios para las nuevas entidades

---
+++ DASI_2025A/Backend/Repositories/OrderRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace Backend
+{
+    public class OrderRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderRepository (ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //  ===== Metodos de Orden ====
+
+        // Crear una orden
+
+
+        // Leer una orden
+
+        // Leer ordenes
+
+        // Actualizar una orden
+
+        // Eliminar una orden
+    }
+}
