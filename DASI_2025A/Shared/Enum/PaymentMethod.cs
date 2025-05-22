29bc7f5da860669b93d2f8f2e673e01025d59731
dennisP <dennis.perez@epn.edu.ec>
2025-05-21 19:45:44 -0500

    Enum para saber el metodo de pago

---
+++ DASI_2025A/Shared/Enum/PaymentMethod.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared;
+
+public enum PaymentMethod
+{
+    AccountBalance,      // Dinero del saldo de la cuenta del usuario
+    BankTransfer,        // Transferencia bancaria
+    PayPal,           // PayPal
+    Cash,               // Efectivo
+}
