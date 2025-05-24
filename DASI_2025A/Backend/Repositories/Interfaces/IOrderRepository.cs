db5110e119b7371d6ae2e2ca692b4289f0a014c4
dennisP <dennis.perez@epn.edu.ec>
2025-05-23 20:10:34 -0500

    Implementacion de Interfaces de OrderRepository, OrderDetailRepository y PaymentRepository

---
+++ DASI_2025A/Backend/Repositories/Interfaces/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using Shared;
+
+namespace Backend
+{
+    public interface IOrderRepository
+    {
+        Task<OrderDto> CreateAsync(OrderDto dto);
+        Task<IEnumerable<OrderDto>> GetAllOrdersAsync();
+        Task<OrderEntity?> GetOrderAsync(int id);
+        Task UpdateOrderAsync(OrderEntity order);
+        Task CancelOrderAsync(OrderEntity order);
+        Task SoftDeleteOrderAsync(OrderEntity order);
+    }
+}
---
+++ DASI_2025A/Backend/Repositories/Interfaces/IPaymentRepository.cs
@@ -0,0 +1,13 @@
+﻿using Shared;
+
+namespace Backend
+{
+    public interface IPaymentRepository
+    {
+        Task<PaymentDto> CreateAsync(PaymentDto paymentDto);
+        Task<IEnumerable<PaymentDto>> GetAllAsync();
+        Task<PaymentEntity?> GetAsync(int paymentId); // para uso interno
+        Task UpdateAsync(PaymentEntity paymentEntity);
+        Task DeleteAsync(PaymentEntity paymentEntity);
+    }
+}
