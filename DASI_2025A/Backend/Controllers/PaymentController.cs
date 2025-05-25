134ec0f682047bba772c101ef29d4a9a3434816a
dennisP <dennis.perez@epn.edu.ec>
2025-05-25 12:27:34 -0500

    Controlador para crar un pago de una orden

---
+++ DASI_2025A/Backend/Controllers/PaymentController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared;
+using System.Security.Claims;
+
+namespace Backend.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly IPaymentService _paymentService;
+
+        public PaymentController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePaymentAsync([FromBody] PaymentDto paymentDto)
+        {
+            try
+            {
+                var response = await _paymentService.CreatePaymentAsync(paymentDto);
+                return Created(string.Empty, response);
+            }
+            catch (BadHttpRequestException ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "Error del servidor al crear el producto." });
+            }
+        }
+    }
+}
