4b587fdff1a0c537ae4d8732ce420ef1ebbc9f61
dennisP <dennis.perez@epn.edu.ec>
2025-05-25 12:29:31 -0500

    Servicio para orden

---
+++ DASI_2025A/Backend/Services/OrderService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared;
+
+namespace Backend
+{
+    public class OrderService : IOrderService
+    {
+        private readonly OrderRepository _orderRepository;
+        private readonly UserRepository _userRepository;
+        
+        public OrderService(OrderRepository orderRepository, UserRepository userRepository)
+        {
+            _orderRepository = orderRepository;
+            _userRepository = userRepository;
+        }
+
+        public async Task<ApiResponse<OrderDto>> CreateOrderAsync(OrderDto orderDto)
+        {
+            var user = await _userRepository.GetAsync(orderDto.UserId);
+            if (user == null)
+            {
+                throw new KeyNotFoundException("No se encontro al Usuario para crear la Orden");
+            }
+
+            var response = await _orderRepository.CreateAsync(orderDto);
+            if (response == null)
+            {
+                throw new BadHttpRequestException("Error al crear la orden");
+            }
+
+            ApiResponse<OrderDto> result = new ApiResponse<OrderDto>(
+                message: "Orden creada exitosamente",
+                data: response,
+                totalRecords: 1
+            );
+
+            return result;
+        }
+    }
+}
