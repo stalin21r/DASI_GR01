d9748508e4dc05b591108c7daac97f60920ee4a0
dennisP <dennis.perez@epn.edu.ec>
2025-05-25 12:30:00 -0500

    Servicio para pago

---
+++ DASI_2025A/Backend/Services/PaymentService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Shared;
+
+namespace Backend
+{
+    public class PaymentService : IPaymentService
+    {
+        private readonly IPaymentRepository _paymentRepository;
+        private readonly IOrderRepository _orderRepository;
+        
+        public PaymentService(IPaymentRepository paymentRepository, IOrderRepository orderRepository)
+        {
+            _paymentRepository = paymentRepository;
+            _orderRepository = orderRepository;
+        }
+
+        // Crear una orden de pago para recargar ek saldo de un usuario
+        public async Task<ApiResponse<PaymentDto>> CreatePaymentAsync(PaymentDto paymentDto)
+        {
+            // Comprobar que la orden existe
+            var order = await _orderRepository.GetOrderAsync(paymentDto.OrderId);
+
+            if (order == null)
+            {
+                throw new KeyNotFoundException("No se encontro la orden para crear el Pago");
+            }
+
+            var response = await _paymentRepository.CreateAsync(paymentDto);
+            ApiResponse<PaymentDto> result = new ApiResponse<PaymentDto>
+            (
+                message: "Pago creado exitosamente",
+                data: response,
+                totalRecords: 1
+
+            );
+
+            return result;
+        }
+    }
+}
